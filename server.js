4e28b6f5f8c2742666788ff29d962a0185ca1080
Justin Williams <justinnwilliams4@gmail.com>
2025-09-18 05:04:30 -0700

    Updated Project

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from ECS Fargate! v1.0\n');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
