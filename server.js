3b87981bd71005892fd85c080e663e5c3d7c2225
Justin Williams <justinnwilliams4@gmail.com>
2025-09-18 05:45:01 -0700

    Change response to HTML format in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from ECS Fargate! v1.0\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<!DOCTYPE html><html><head><title>ECS Fargate</title></head><body style="background:#f8f9fa;text-align:center;padding-top:50px;"><h1>Hello from ECS Fargate! v1.0</h1></body></html>');
 });
 
 server.listen(8080, () => {
